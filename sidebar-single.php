c3f9d888b107840726f5015f0908f0c95ed4161f
Wang <894056459@qq.com>
2019-11-27 16:10:35 +0800

    新增文章侧边栏

---
+++ sidebar-single.php
@@ -0,0 +1,9 @@
+	<!-- 文章侧边栏 -->
+<aside>
+	<?php if( !function_exists('dynamic_sidebar')||!dynamic_sidebar('文章侧边栏')):?>
+		<h4>自定义小工具</h4>
+		<ul>
+			<li>自定义小工具</li>
+		</ul>
+		<?php endif; ?>
+</aside>
